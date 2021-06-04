0e0d5347cf3ffb080c87dbf25de814e977978cf3
abdelnabut <AbdelNabut@outlook.com>
2021-06-03 21:32:06 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.math.BigDecimal;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Switched from primitive data types to the BigDecimal class to handle currencies with precision.
+        BigDecimal principle, years;
+        BigDecimal rate, percentRate, value;
+        Scanner input = new Scanner(System.in);
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        System.out.print( "Enter the principle: " );
+        principle = input.nextBigDecimal();
+        System.out.print( "Enter the % rate of interest : " );
+        percentRate = input.nextBigDecimal();
+        rate = percentRate.divide(BigDecimal.valueOf(100));
+        System.out.print( "Enter the number of years: " );
+        years = input.nextBigDecimal();
+        value = principle.multiply((BigDecimal.valueOf(1).add((rate.multiply(years)))));
+        System.out.println("After " + years + " years at " + percentRate
+                + "%, the investment will be worth " + formatter.format(value));
+
+
+    }
+}
