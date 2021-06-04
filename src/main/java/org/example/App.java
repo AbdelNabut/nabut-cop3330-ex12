7e4343dc9d718d6a8bcd55c2760ee287c540178d
abdelnabut <AbdelNabut@outlook.com>
2021-06-04 00:30:49 -0400

    Added penny-rounding

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.math.RoundingMode;
 import java.util.Scanner;
 import java.text.NumberFormat;
 import java.math.BigDecimal;
@@ -19,7 +20,9 @@ public class App
         rate = percentRate.divide(BigDecimal.valueOf(100));
         System.out.print( "Enter the number of years: " );
         years = input.nextBigDecimal();
-        value = principle.multiply((BigDecimal.valueOf(1).add((rate.multiply(years)))));
+        value = principle
+                .multiply((BigDecimal.valueOf(1).add((rate.multiply(years)))))
+                .setScale(2, RoundingMode.HALF_UP);;
         System.out.println("After " + years + " years at " + percentRate
                 + "%, the investment will be worth " + formatter.format(value));
 
